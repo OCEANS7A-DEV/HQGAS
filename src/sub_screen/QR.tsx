e1c1915eee6d891c6c6421189b87155bf4cffefc
aristos <storm1legendaryhero@gmail.com>
2024-12-25 14:35:02 +0900

    shuusei

--- src/sub_screen/QR.tsx
+++ src/sub_screen/QR.tsx
@@ -1,18 +1,29 @@
-import React, { useRef, useEffect, useState } from 'react';
-import '../css/QRBuild.css'
+import React, { useEffect, useState } from 'react';
+import '../css/QRBuild.css';
 
 const QRBuild = () => {
   const [data, setData] = useState([]);
-  const [QRData, setQRData] = useState([]);
-  
-  const QRDATA = (e, data) => {
-    //if(e.chackbox.checked){}
-    console.log(e.checkbox)
+  const [selectedRows, setSelectedRows] = useState<string[]>([]);  // 選択した行の管理
+
+  // チェックボックスが変更されたときの処理
+  const handleCheckboxChange = (e, row) => {
+    if (e.target.checked) {
+      setSelectedRows((prev) => [...prev, row]);  // 選択された行を追加
+    } else {
+      setSelectedRows((prev) => prev.filter((item) => item !== row));  // 選択解除
+    }
   };
 
+  // QRコード作成ボタンを押したときの処理
+  const handleGenerateQR = () => {
+    console.log('選択された行のデータ:', selectedRows);
+    // QRコード生成処理や別のロジックを追加
+  };
+
+  // ローカルストレージからデータを取得
   useEffect(() => {
-    const setdata = JSON.parse(localStorage.getItem('data'));
-    setData(setdata);
+    const storedData = JSON.parse(localStorage.getItem('data'));
+    setData(storedData || []);
   }, []);
 
   return (
@@ -26,7 +37,7 @@ const QRBuild = () => {
             <table className="data-table-head">
               <thead>
                 <tr>
-                  <th className="chackbox">選択</th>
+                  <th className="checkbox">選択</th>
                   <th className="qr-vendor">業者名</th>
                   <th className="qr-code">商品コード</th>
                   <th className="qr-name">商品名</th>
@@ -37,7 +48,12 @@ const QRBuild = () => {
                 {
                   data.map((row, index) => (
                     <tr key={index}>
-                      <td className="chackbox"><input type="checkbox" onClick={(e) => QRDATA(e,row)}></input></td>
+                      <td className="checkbox">
+                        <input
+                          type="checkbox"
+                          onChange={(e) => handleCheckboxChange(e, row)}
+                        />
+                      </td>
                       <td className="qr-vendor-td">{row[0]}</td>
                       <td className="qr-code-td">{row[1]}</td>
                       <td className="qr-name-td">{row[2]}</td>
@@ -49,12 +65,17 @@ const QRBuild = () => {
             </table>
           </div>
           <div className="QRButton">
-            <a className="buttonUnderlineSt" type="button" onClick={console.log(QRData)}>QRコード作成</a>
+            <a
+              className="buttonUnderlineSt"
+              type="button"
+              onClick={handleGenerateQR}  // ボタンクリックでQRコード生成
+            >
+              QRコード作成
+            </a>
           </div>
         </div>
       </div>
     </div>
-    
   );
 };
 
