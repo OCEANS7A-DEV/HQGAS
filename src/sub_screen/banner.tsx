7196b6731929da840dbad3905dfda995bf87a84a
OCEANS7A-DEV <oceans7a@gmail.com>
2025-01-08 15:24:09 +0900

    print削除

--- src/sub_screen/banner.tsx
+++ src/sub_screen/banner.tsx
@@ -49,9 +49,9 @@ export default function TopBanner({ setCurrentPage }: SettingProps) {
         <a className="buttonUnderline" id="main_back" type="button" onClick={() => clickpage('HQPage')}>
           印刷関係
         </a>
-        <a className="buttonUnderline" id="main_back" type="button" onClick={() => clickpage('KinbatoPrintPage')}>
+        {/* <a className="buttonUnderline" id="main_back" type="button" onClick={() => clickpage('KinbatoPrintPage')}>
           print
-        </a>
+        </a> */}
       </div>
     </div>
   );
